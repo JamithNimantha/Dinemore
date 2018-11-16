2c1011320926c554d295540cb8360774f531474e
jamith <jamithnimantha@gmail.com>
2018-11-17 04:30:08 +0530

    manage Menu pane created

--- Dinemore-Client/src/lk/ijse/dinemore/controller/admin/AdminDashController.java
+++ Dinemore-Client/src/lk/ijse/dinemore/controller/admin/AdminDashController.java
@@ -1,4 +1,75 @@
 package lk.ijse.dinemore.controller.admin;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminDashController {
+
+    @FXML
+    private JFXButton btnMenu;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    @FXML
+    private JFXButton btnDash;
+
+    @FXML
+    private JFXButton btnCustomer;
+
+    @FXML
+    private JFXButton btnTelOp;
+
+    @FXML
+    private JFXButton btnChef;
+
+    @FXML
+    private JFXButton btnRider;
+
+    @FXML
+    private JFXButton btnReport;
+
+    @FXML
+    private JFXButton btnSettings;
+
+    @FXML
+    private AnchorPane pnlMain;
+
+    @FXML
+    void btnChefOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMenuOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRiderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettingsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnTelOpOnAction(ActionEvent event) {
+
+    }
+
 }

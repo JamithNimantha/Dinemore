640448722b8757a9b23766a7d7b2f842b9cbed9d
jamith <jamithnimantha@gmail.com>
2018-11-21 08:00:24 +0530

    Created ChefBO

---
+++ Dinemore-Server/src/lk/ijse/dinemore/business/Custom/ChefBO.java
@@ -0,0 +1,17 @@
+package lk.ijse.dinemore.business.Custom;
+
+import lk.ijse.dinemore.business.SuperBO;
+import lk.ijse.dinemore.model.ChefDTO;
+
+import java.util.List;
+
+public interface ChefBO extends SuperBO {
+    public boolean addChef(ChefDTO chefDTO) throws Exception;
+
+    public boolean updateChef(ChefDTO chefDTO) throws Exception;
+
+    public boolean deleteChef(String id) throws Exception;
+
+    public List<ChefDTO> getAllChefs() throws Exception;
+
+}

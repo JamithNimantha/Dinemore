9195393b7415bd6e2d35499df22d541e333e2aa1
jamith <jamithnimantha@gmail.com>
2018-11-21 07:53:51 +0530

    Created SuperBO

---
+++ Dinemore-Server/src/lk/ijse/dinemore/service/custom/impl/ChefServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.dinemore.service.custom.impl;
+
+import lk.ijse.dinemore.observer.Subject;
+import lk.ijse.dinemore.service.custom.ChefService;
+
+import java.rmi.server.UnicastRemoteObject;
+
+public class ChefServiceImpl extends UnicastRemoteObject implements Subject, ChefService {
+
+}

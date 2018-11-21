0649584d817467094606f53f6294a7919eef7895
jamith <jamithnimantha@gmail.com>
2018-11-21 08:13:17 +0530

    Created ChefRepositary

---
+++ Dinemore-Server/src/lk/ijse/dinemore/respository/custom/ChefRepository.java
@@ -0,0 +1,8 @@
+package lk.ijse.dinemore.respository.custom;
+
+import lk.ijse.dinemore.entity.Chef;
+import lk.ijse.dinemore.respository.SuperRepository;
+
+public interface ChefRepository extends SuperRepository<Chef, String> {
+
+}
